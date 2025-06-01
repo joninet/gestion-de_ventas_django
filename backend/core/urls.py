c7d5fc30d62f47546e97cd130fab4df4af13cebf
jonathan <jon.desplats@gmail.com>
2025-05-31 22:30:18 -0300

    create put, delete, get, post product

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('products/', views.productList, name='product-list'),
+    path('products/<int:pk>/', views.productDetail, name='product-detail'),
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,4 @@
+from ast import Try
 from sys import api_version
 
 from django.shortcuts import render
@@ -24,3 +25,28 @@ def productList(request):
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=200)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def productDetail(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+
+    except Product.DoesNotExist:
+        return Response({'error': 'Product not found'}, status=404)
+
+    if request.method == 'GET':
+        serializer = ProductSerializer(product, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=200)
+
+    elif request.method == 'DELETE':
+        #delete product
+        product.delete()
+        return Response(status=204)
+
