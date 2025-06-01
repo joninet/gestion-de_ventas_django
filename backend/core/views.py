e17c87242c8b7e58825fd76d4b5246053e89da8c
jonathan <jon.desplats@gmail.com>
2025-05-31 22:04:15 -0300

    create view and url product

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,26 @@
+from sys import api_version
+
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Product
+from .serializers import ProductSerializer
+
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+def productList(request):
+    if request.method == 'GET':
+        #obtenemos todos los productos
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    #creamos un nuevo producto
+    if request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=200)
